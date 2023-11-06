b583303f505aa33ebea5c4365ea15254acc841bd
Greg Kreisman <gkreisman_cep@qq.com>
2023-11-06 21:43:29 +0800

    change background to white for ai

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   cx = (windowWidth - width) / 2;
   cy = (windowHeight - height) / 2;
   cnv.position(cx, cy);
-  background("#30303a");
+  background(255);//background("#30303a");
   mycheck = createCheckbox("dynamic if checked", true);
   mycheck.position(cx,cy+height+20);
   mylink = createA('https://github.com/greggelong/dynamicMondrian', 'link to this  repo');
